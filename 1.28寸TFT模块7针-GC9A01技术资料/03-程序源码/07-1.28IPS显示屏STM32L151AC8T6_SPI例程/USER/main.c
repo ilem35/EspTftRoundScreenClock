75e2e274a579a65c3d957fb8341e3fb53b2791b9
ilem35 <102783767+ilem35@users.noreply.github.com>
2023-02-09 16:08:37 +0800

    Add files via upload

---
+++ 1.28寸TFT模块7针-GC9A01技术资料/03-程序源码/07-1.28IPS显示屏STM32L151AC8T6_SPI例程/USER/main.c
@@ -0,0 +1,69 @@
+//////////////////////////////////////////////////////////////////////////////////	 
+//本程序只供学习使用，未经作者许可，不得用于其它任何用途
+//中景园电子
+//店铺地址：http://shop73023976.taobao.com
+//
+//  文 件 名   : main.c
+//  版 本 号   : v2.0
+//  作    者   : HuangKai
+//  生成日期   : 2018-0101
+//  最近修改   : 
+//  功能描述   :
+//              说明: 
+//              ----------------------------------------------------------------
+//              GND   电源地
+//              VCC   3.3v电源
+//              SCL   PA0（SCLK）
+//              SDA   PA1（MOSI）
+//              RES   PA2
+//              DC    PA3
+//              CS    PA4
+//              BLK   PA5
+//              ----------------------------------------------------------------
+// 修改历史   :
+// 日    期   : 
+// 作    者   : HuangKai
+// 修改内容   : 创建文件
+//版权所有，盗版必究。
+//Copyright(C) 中景园电子2018
+//All rights reserved
+//******************************************************************************/
+
+#include "main.h"
+#include "delay.h"
+#include "sys.h"
+#include "led.h"
+#include "lcd_init.h"
+#include "lcd.h"
+#include "pic.h"
+
+int main()
+{
+	u8 i,j;
+	float t=0;
+	HAL_Init();
+	SystemClock_Config();
+  LED_Init();//LED初始化
+	LCD_Init();//LCD初始化
+	LCD_Fill(0,0,LCD_W,LCD_H,WHITE);
+	LED_ON;
+	while(1)
+	{
+		LCD_ShowChinese(30,40,"中景园电子",RED,WHITE,32,0);
+		LCD_ShowString(32,80,"LCD_Diameter:",RED,WHITE,16,0);
+		LCD_ShowIntNum(134,80,LCD_W,3,RED,WHITE,16);
+		LCD_ShowString(32,100,"Increaseing Nun:",RED,WHITE,16,0);
+		LCD_ShowFloatNum1(160,100,t,4,RED,WHITE,16);
+		t+=0.11;
+		for(j=0;j<3;j++)
+		{
+			for(i=0;i<6;i++)
+			{
+				LCD_ShowPicture(40*i,120+j*40,40,40,gImage_1);
+			}
+		}
+	}
+}
+
+
+
